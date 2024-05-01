fe99ec3e0218b0317d91d6b23a0dd491b4f472ce
Dinithi Gamage <51855634+diniSSG@users.noreply.github.com>
2024-05-02 01:42:01 +0530

    Add files via upload

---
+++ Authentication/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDSafe.Authentication
+{
+	public class RegisterModel
+	{
+	}
+}
